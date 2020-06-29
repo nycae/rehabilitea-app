fc11b35c27f2863a09ad797cb8f3071ae309c26b
Roberto Plaza Romero <36627781+RoberPlaza@users.noreply.github.com>
2020-06-29 20:23:11 +0200

    Added screen flashing

--- Assets/Code/Scripts/HideAndSeek/UIManager.cs
+++ Assets/Code/Scripts/HideAndSeek/UIManager.cs
@@ -5,13 +5,35 @@ using UnityEngine.UI;
 
 namespace HideAndSeek 
 {
-    public class UIProgressBar : MonoBehaviour
+    public class UIManager : MonoBehaviour
     {
         [SerializeField] private Slider                 slider      = null;
         [SerializeField] private HideAndSeekGameMode    gameMode    = null;
         [SerializeField] private RehabiliTEA.Player     player      = null;
+        [SerializeField] private CanvasGroup            flashScreen = null;
         [SerializeField] private GameObject             optionsMenu = null;
                          private float                  timestamp   = 0f;   
+
+        void FlashMessage(bool wasSuccessfull)
+        {
+            flashScreen.GetComponentInChildren<Text>().text = (wasSuccessfull) ? "¡Bien hecho!" : "¡Prueba otra vez!" ;
+            DisplayMessage();
+            StartCoroutine("FadeOutMessage");
+        }
+
+        void DisplayMessage()
+        {
+            flashScreen.alpha = 1f;
+        }
+
+        IEnumerator FadeOutMessage()
+        {
+            for (float f = 1f; f > 0; f -= 0.005f)
+            {
+                flashScreen.alpha = f;
+                yield return new WaitForSeconds(0.005f);
+            }
+        }
 
         void ResetTimestamp(GameObject gameObject = null)
         {
@@ -29,6 +51,8 @@ namespace HideAndSeek
         {
             player.OnSelect     += ResetTimestamp;
             gameMode.OnGameEnd  += HideBar;
+            gameMode.OnNewRound += FlashMessage;
+            flashScreen.alpha   = 0f;
 
             optionsMenu.SetActive(false);
             ResetTimestamp();
